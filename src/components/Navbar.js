74af8d4b5c3c92835a11f343aae3ed35aef50416
Mahmoud Ektefaie <ektefaie@gmail.com>
2022-05-10 00:04:34 -0700

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <div className='navbar__bg'>
+      <h1>
+        <i class='fa fa-globe' aria-hidden='true'></i>
+        &ensp;My Travel Journal
+      </h1>
+    </div>
+  );
+}
